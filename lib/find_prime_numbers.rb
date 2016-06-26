8a2098f422619037c68cb6d4a84cb596f4b9641d
Rachel Lyons <rlyons21@hotmail.com>
2016-06-25 22:15:45 -0500

    completed assignment

--- lib/find_prime_numbers.rb
+++ lib/find_prime_numbers.rb
@@ -2,4 +2,16 @@
 # returns a properly formatted phone number.
 
 def find_prime_numbers(num)
+	arr =[]
+    (2..num).each{|x| arr.push(x)}
+
+    i = 0
+    p = arr[i]
+
+    while p != arr[-1]
+	    arr.delete_if{|n| n%p == 0 && n != p}
+	    i+= 1
+	    p = arr[i]
+    end
+    return arr
 end

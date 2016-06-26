ccff324a0687e97c5175056ee92a14fa9244647f
Blair Englund <blair.englund@gmail.com>
2016-06-25 21:49:11 -0500

    tweaks

--- lib/find_prime_numbers.rb
+++ lib/find_prime_numbers.rb
@@ -6,7 +6,7 @@ def find_prime_numbers(num)
 	primes = Array.new
 	prange.each do |i| #I want to remove items if they have more than two factors. I can achieve this by removing all multiples of two and then all multiples of three and so on.
 		primes.push(i) #adds the current int to the array of primes, next I want to delete any multiples of i
-		(2..num).each do |x| #the each loop will go through and remove multiple
+		(2..num).each do |x| #the each loop will go through and remove multiples of i by multiplying them by each number in range
 			prange.delete(x*i) #deletes numbers in prange
 		end
 	end

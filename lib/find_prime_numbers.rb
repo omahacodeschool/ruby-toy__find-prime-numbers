614149ad6a8ed2cc4e761eb70eb7bb170ce24065
John Morrow <CodeMorrow@yahoo.com>
2016-06-26 19:08:52 -0500

    Completed Prime numbers.

--- lib/find_prime_numbers.rb
+++ lib/find_prime_numbers.rb
@@ -3,4 +3,17 @@
 
 def find_prime_numbers(num)
 	
+	primes = (2..num).to_a
+		#set each vaule to two different variales
+		(2..num).each do |c|
+			(2..num).each do |n|
+				#If the value is divisible by another value in the array and the value does nto equal itself..
+				if (c % n ==0 && n != c)
+					#delete that number from the array.
+					primes.delete(c)
+				break
+			end
+		end
+	end
+	return primes
 end

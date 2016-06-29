6acd9c38d49b44c3a324c8a0bf66e45a8a9b05de
Alexander Noto <noto.alex.j@gmail.com>
2016-06-29 12:54:57 -0500

    Completed exercise

--- lib/find_prime_numbers.rb
+++ lib/find_prime_numbers.rb
@@ -2,4 +2,17 @@
 # returns an array of all the prime numbers in a range 1..n
 
 def find_prime_numbers(num)
+	original = (2..num).to_a
+	nonprimes = []
+
+	(2..num).each do |divisor|
+		original.each do |nonprime|
+			if nonprime % divisor == 0 
+				if nonprime != divisor
+					nonprimes.push(nonprime)
+				end
+			end
+		end
+	end
+	return original - nonprimes
 end

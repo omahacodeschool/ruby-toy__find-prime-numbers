174e3e5410cc5900aadcc705bdad5e017a5deecc
Matt Hughes <mhughes27@gmail.com>
2016-06-30 22:17:18 -0500

    tests pass

--- lib/find_prime_numbers.rb
+++ lib/find_prime_numbers.rb
@@ -2,4 +2,15 @@
 # returns a properly formatted phone number.
 
 def find_prime_numbers(num)
+	interval = (2..num).to_a
+	composites = []
+
+	interval.each do |x|
+		interval.each do |composite|
+			if composite % x == 0 && composite != x
+					composites.push(composite)
+			end
+		end
+	end
+	return interval - composites
 end
